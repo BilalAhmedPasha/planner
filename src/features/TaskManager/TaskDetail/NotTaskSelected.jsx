b272a74e3d9171037dcb959a81cd980d75140c50
Bilal Ahmed Pasha <bilalahmed3120@gmail.com>
2023-02-17 00:49:57 +0530

    View detail page error fix

--- src/features/TaskManager/TaskDetail/NotTaskSelected.jsx
+++ src/features/TaskManager/TaskDetail/NotTaskSelected.jsx
@@ -1,17 +1,29 @@
 import { Typography } from "antd";
+import noSelection from "../../../assets/noSelection.png";
+import multiSelection from "../../../assets/multiSelection.png";
 
-const NotTaskSelected = () => {
+const NotTaskSelected = ({ selectedTaskDetails }) => {
   return (
     <div
       style={{
         textAlign: "center",
-        display: "flex",
-        justifyContent: "center",
+        paddingTop: "40%",
       }}
     >
-      <Typography.Title level={5}>
-        Select a task to view details
-      </Typography.Title>
+      <div>
+        <img
+          src={selectedTaskDetails.length > 1 ? multiSelection : noSelection}
+          alt="DetailsNotFound"
+          style={{
+            width: "5rem",
+          }}
+        />
+      </div>
+      <Typography.Text style={{ fontSize: "1rem" }} type="secondary">
+        {selectedTaskDetails.length > 1
+          ? "Select only a single task to view details"
+          : "Click on a task to view details"}
+      </Typography.Text>
     </div>
   );
 };
